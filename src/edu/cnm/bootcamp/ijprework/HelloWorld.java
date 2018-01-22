6cfd66d61e2386bf129cc3b2d63ef41ebc95d38c
donny <donnystout47@gmail.com>
2018-01-22 15:52:43 -0700

    Added new salutations and changed the method.

--- src/edu/cnm/bootcamp/ijprework/HelloWorld.java
+++ src/edu/cnm/bootcamp/ijprework/HelloWorld.java
@@ -3,12 +3,23 @@ package edu.cnm.bootcamp.ijprework;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    salute(salutation(args));
+    String target = salutees(args);
+    String message = salutation(target);
+    salute(message);
+    }
+
+  private static String salutees(String[] args) {
+    String result = "";
+    for (int i = 0; i < args.length; i++) {
+      result = result + args[i] + ", " ;
+    }
+    return result;
   }
 
-  private static String salutation(String[] args) {
-    return "Hello, " + ((args.length > 0) ? args[0] : "World");
+  private static String salutation(String target) {
+    return "Hello, " + (!target.isEmpty() ? target : "World");
   }
+
   private static void salute(String salutation) {
     System.out.println(salutation);
   }
